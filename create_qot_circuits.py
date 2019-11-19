a75cfd43b6836009442993779add66da2839654f
tehruhn <trolldemort9@gmail.com>
2019-11-19 18:06:39 +0900

    implemented complete measurements in separate file

---
+++ create_qot_circuits.py
@@ -0,0 +1,104 @@
+import qiskit
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from copy import deepcopy
+
+
+def create_qot_circuits(quantum_circuit, hash_functions, k=2):
+    qubit_count = quantum_circuit.n_qubits
+    qcx = deepcopy(quantum_circuit)
+    qcx.name = "all_x_basis"
+    qcy = deepcopy(quantum_circuit)
+    qcy.name = "all_y_basis"
+    qcz = deepcopy(quantum_circuit)
+    qcz.name = "all_z_basis"
+
+    # step 1 in the paper
+
+    # measure in x-basis
+    for i in range(qubit_count):
+        qcx.h(i)
+    qcx.measure(range(qubit_count), range(qubit_count))
+    # measure in the y-basis
+    for i in range(qubit_count):
+        qcy.sdg(i)
+        qcy.h(i)
+    qcy.measure(range(qubit_count), range(qubit_count))
+    # measure in the z-basis
+    qcz.measure(range(qubit_count), range(qubit_count))
+
+    # step 2 in the paper
+
+    name = ""
+    circuit_grid = []
+    # for each hash function
+    # naming convention
+    # hashfnnumber_measrurementnber
+    for i in range(len(hash_functions)):
+        temp_list = []
+        thisname = name + str(i) + "_"
+        # measurement 1
+        m1 = deepcopy(quantum_circuit)
+        for j in range(qubit_count):
+            if hash_functions[i][j]:
+                m1.sdg(i)
+                m1.h(i)
+            else:
+                m1.h(i)
+        m1.measure(range(qubit_count), range(qubit_count))
+        m1.name = thisname + "0"
+
+        # measurement 2
+        m2 = deepcopy(quantum_circuit)
+        for j in range(qubit_count):
+            if hash_functions[i][j]:
+                m2.h(i)
+            else:
+                m2.sdg(i)
+                m2.h(i)
+        m2.measure(range(qubit_count), range(qubit_count))
+        m2.name = thisname + "1"
+
+        # measurement 3
+        m3 = deepcopy(quantum_circuit)
+        for j in range(qubit_count):
+            if hash_functions[i][j] == 0:
+                m3.h(i)
+        m3.measure(range(qubit_count), range(qubit_count))
+        m3.name = thisname + "2"
+
+        # measurement 4
+        m4 = deepcopy(quantum_circuit)
+        for j in range(qubit_count):
+            if hash_functions[i][j]:
+                m4.h(i)
+        m4.measure(range(qubit_count), range(qubit_count))
+        m4.name = thisname + "3"
+
+        # measurement 5
+        m5 = deepcopy(quantum_circuit)
+        for j in range(qubit_count):
+            if hash_functions[i][j] == 0:
+                m5.sdg(i)
+                m5.h(i)
+        m5.measure(range(qubit_count), range(qubit_count))
+        m5.name = thisname + "4"
+
+        # measurement 6
+        m6 = deepcopy(quantum_circuit)
+        for j in range(qubit_count):
+            if hash_functions[i][j]:
+                m6.sdg(i)
+                m6.h(i)
+        m6.measure(range(qubit_count), range(qubit_count))
+        m6.name = thisname + "5"
+
+        temp_list.append(m1)
+        temp_list.append(m2)
+        temp_list.append(m3)
+        temp_list.append(m4)
+        temp_list.append(m5)
+        temp_list.append(m6)
+
+        circuit_grid.append(temp_list)
+
+    return qcx, qcy, qcz, circuit_grid
