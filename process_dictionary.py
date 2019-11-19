87b71e1fd1b2dbb43ae9a5b085da20f77365d757
tehruhn <trolldemort9@gmail.com>
2019-11-19 23:02:01 +0900

    all 15 computations done

--- process_dictionary.py
+++ process_dictionary.py
@@ -110,6 +110,60 @@ def process_dictionary_ix_iy_iz(circuit_name, output):
 
 	return answer
 
+def process_dictionary_xi_yi_zi(circuit_name, output):
+	dictionary = output[circuit_name]
+	list_of_keys = list(dictionary.keys())
+	# print(list_of_keys)
+	num_qubits = len(list_of_keys[0])
+	# print(num_qubits)
+	# list of numbers to iterate on
+	list_of_nums = list(range(num_qubits))
+	answer = []
+	for i in itertools.combinations(list_of_nums, 2):
+		# find two indices to fix
+		first = i[0]
+		second = i[1]
+		# fix 00 and sum
+		zz = []
+		s1 = 0
+		for j in list_of_keys:
+			if j[first] == "0" and j[second] == "0":
+				zz.append(j)
+		for k in zz:
+			s1 += dictionary[k]
+		# fix 01 and sum 
+		zo = []
+		s2 = 0
+		for j in list_of_keys:
+			if j[first] == "0" and j[second] == "1":
+				zo.append(j)
+		for k in zo:
+			s2 += dictionary[k]
+
+		# fix 10 and sum
+		oz = []
+		s3 = 0
+		for j in list_of_keys:
+			if j[first] == "1" and j[second] == "0":
+				oz.append(j)
+		for k in oz:
+			s3 += dictionary[k]
+
+		# fix 11 and sum
+		oo = []
+		s4 = 0
+		for j in list_of_keys:
+			if j[first] == "1" and j[second] == "1":
+				oo.append(j)
+		for k in oo:
+			s4 += dictionary[k]
+
+		total = (s4 + s3 - s2 - s1)/1024
+		# print(s1, s2, s3, s4)
+		answer.append(total)
+
+	return answer
+
 def process_dictionary_xy12(num_qubits, output, hash_functions):
 	# list of numbers to iterate on
 	list_of_nums = list(range(num_qubits))
